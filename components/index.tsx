4b52f088d224be6251ad2d415d7734244bd9eb36
ngirimana <chadrackngirimana@gmail.com>
2023-06-13 22:31:19 +0200

    hero section

---
+++ components/index.tsx
@@ -0,0 +1,11 @@
+import Hero from "@/components/Hero";
+import CustomButton from "@/components/CustomButton";
+import NavBar from "@/components/NavBar";
+import Footer from "@/components/Footer";
+
+export {
+    Hero,
+    CustomButton,
+    NavBar,
+    Footer
+}
